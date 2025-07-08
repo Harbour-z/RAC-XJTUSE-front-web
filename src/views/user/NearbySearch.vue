88856bd836870568676c84cf8e5b596e9d493b4d
glonor <2802846561@qq.com>
2025-07-08 17:19:57 +0800

    <feat>增加了地图显示功能，在附近搜索界面

--- src/views/user/NearbySearch.vue
+++ src/views/user/NearbySearch.vue
@@ -1,15 +1,25 @@
 <script lang="ts" setup>
 
+
+
 </script>
 
 <template>
-  <div>
-    <el-card>
-      <h1>附近搜索</h1>
+  <div >
+    <el-card >
+      附近搜索
     </el-card>
   </div>
+  <div>
+    <baidu-map class="bm-view" :zoom="12" :center="{lng: 116.404, lat: 39.915}" >
+    </baidu-map>
+  </div>
+
 </template>
 
-<style scoped lang="scss">
-
+<style>
+.bm-view {
+  width: 100%;
+  height: 300px;
+}
 </style>
