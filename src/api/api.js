3e56fd83e1185bc5fff27e4621b7cd687d301a8f
ABSXDIVY <2017078173@qq.com>
2025-07-08 11:26:00 +0800

    fix:重新适配数据库

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,9 @@ import request from "@/utils/request";
 
 //登录
 //传入用户名与密码
-export const userLogin = (query) => request.post('/api/user/login',query);
-export const merchantLogin = (query) => request.post('/api/merchant/login',query);
-export const adminLogin = (query) => request.post('/api/admin/login',query);
+export const userLogin = (query) => request.post('/api/userInfo/login',query);
+export const merchantLogin = (query) => request.post('/api/merchantInfo/login',query);
+export const adminLogin = (query) => request.post('/api/adminInfo/login',query);
 
 //url?msg="你好"
 export const aiChat = (query) => request.get('/api/ai/chat1',{params:query})
