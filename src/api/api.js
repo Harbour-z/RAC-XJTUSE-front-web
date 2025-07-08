81037a2c61dfdad47f1bc06a7d8e85ea6f0f5e6a
ABSXDIVY <2017078173@qq.com>
2025-07-08 15:34:48 +0800

    fix:注册api的Bug

--- src/api/api.js
+++ src/api/api.js
@@ -8,7 +8,7 @@ export const adminLogin = (query) => request.post('/api/adminInfo/login',query);
 
 //注册
 export const userRegister = (query) => request.post('/api/userInfo/register',query);
-export const merchantRegister = (query) => request.post('/api/userInfo/register',query);
+export const merchantRegister = (query) => request.post('/api/merchantInfo/register',query);
 
 //url?msg="你好"
 export const aiChat = (query) => request.get('/api/ai/chat1',{params:query})
