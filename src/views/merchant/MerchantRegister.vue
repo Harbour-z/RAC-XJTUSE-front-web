8e0eae372b4ce2aabc8ae76ecd282100b10b45fb
Zion Cheng郑苏航 <3373391470@qq.com>
2025-07-09 17:26:25 +0800

    前端注册商户参数名字的统一

--- src/views/merchant/MerchantRegister.vue
+++ src/views/merchant/MerchantRegister.vue
@@ -9,7 +9,7 @@ const form = reactive({
   merchantName: '',
   username:'',
   licenseNumber: '',
-  type: [],
+  categoryId: [],
   description: '',
 })
 //提交注册表
@@ -79,23 +79,10 @@ const textarea2 = ref('')
 //许可证图片栏
 import { ElMessage } from 'element-plus'
 const imageUrl = ref('')
-const handleAvatarSuccess: UploadProps['onSuccess'] = (
-    response,
-    uploadFile
-) => {
+const handleAvatarSuccess = (response, uploadFile) => {
   imageUrl.value = URL.createObjectURL(uploadFile.raw!)
 }
 
-const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
-  if (rawFile.type !== 'image/jpeg') {
-    ElMessage.error('Avatar picture must be JPG format!')
-    return false
-  } else if (rawFile.size / 1024 / 1024 > 2) {
-    ElMessage.error('Avatar picture size can not exceed 2MB!')
-    return false
-  }
-  return true
-}
 </script>
 
 <template>
@@ -122,10 +109,9 @@ const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
           <el-form-item label="许可证图片">
             <el-upload
               class="avatar-uploader"
-              action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+              action="/api/shop/register"
               :show-file-list="false"
               :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload"
           >
             <img v-if="imageUrl" :src="imageUrl" class="avatar" />
             <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
@@ -186,7 +172,7 @@ const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
 <!--            <el-switch v-model="form.delivery" />-->
 <!--          </el-form-item>-->
           <el-form-item label="商品类别">
-            <el-checkbox-group v-model="form.type">
+            <el-checkbox-group v-model="form.categoryId">
               <el-checkbox value="中餐" name="type">
                 中餐
               </el-checkbox>
